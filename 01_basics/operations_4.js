7a840c71d7d1f3990a4f8375044f8baa6cb39e14
sah-lishi <158027503+sah-lishi@users.noreply.github.com>
2025-05-17 14:34:05 +0000

    operations on data

---
+++ 01_basics/operations_4.js
@@ -0,0 +1,17 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+console.log(2**3);
+
+let str1 = "hello"
+let str2 = "kitty"
+let str3 = str1 + str2
+console.log(str3);
+console.log("1" + 2 + 6);
+
+console.log(1 + 4 + "4");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+
