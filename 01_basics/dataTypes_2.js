44b06d93635747689c03cad5c1a97d1b0ab56d9f
sah-lishi <158027503+sah-lishi@users.noreply.github.com>
2025-05-17 08:37:56 +0000

    var and datatypes

---
+++ 01_basics/dataTypes_2.js
@@ -0,0 +1,7 @@
+"use strict" //treat all JS code as newer version
+// alert(2+3)   // as we are using node js not browser
+console.log(3*3);
+
+console.log(typeof undefined);
+console.log(typeof null); //object
+
---
+++ 01_basics/variables_1.js
@@ -0,0 +1,12 @@
+const accountId = 76382
+let accountEmail = "lishi@gmail.com"
+var accountPassword = "76371"
+accountCity = "Purnia"
+let accountState;
+// accountId = 3
+accountEmail = "shreen@gmail.com"
+accountPassword = "hhsd23"
+accountCity = "Kolkata"
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
