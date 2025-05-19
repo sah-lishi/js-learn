8e3d01ab80eec4d29d6c0eeef8e9e10e9a17c176
sah-lishi <158027503+sah-lishi@users.noreply.github.com>
2025-05-19 08:29:10 +0000

    stack AND heap

---
+++ 01_basics/stackHeap.js
@@ -0,0 +1,16 @@
+// Stack(primitive) Heap(non - primitive)
+ let myName = "Lishi"
+ let yourName = myName
+ yourName = "Shreya"
+ console.log(yourName);
+ console.log(myName);
+
+ let myClass = {
+    name: "Lishi Sah",
+    age: 12
+ }
+ let yourClass = myClass
+ yourClass.name = "Shreya Sah"
+  console.log(myClass.name);
+  
+ 
