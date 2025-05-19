46e3b99e658a8622cdd34342636f5dba56b10a3a
sah-lishi <158027503+sah-lishi@users.noreply.github.com>
2025-05-19 06:53:28 +0000

    primitive and non primitve data types

--- 01_basics/dataTypes_5.js
+++ 01_basics/dataTypes_5.js
@@ -1,5 +1,25 @@
 // primitive data types
 // String, Number, Boolean, null, undefined, Symbol, BigInt
+const score = 5
+const isLogged = false
+let userEmail;
+
+const id = Symbol('234')
+const secId = Symbol('234')
+console.log(id === secId);
+console.log(typeof secId);
+
 
 // Reference(non primitive) data type
 // Array, Objects, Functions
+const fruits = ["mango", "guava", "watermelon"]
+console.log(fruits);
+
+let myObject = {
+    name: "Lishi",
+    age: "59"
+}
+
+const myFunction = function () {
+    console.log("Hello");
+}
