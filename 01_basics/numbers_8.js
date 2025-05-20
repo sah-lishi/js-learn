bedd9cbcc3797516f860ba10168cbeb1454f5ebc
sah-lishi <158027503+sah-lishi@users.noreply.github.com>
2025-05-20 11:06:13 +0000

    numbers

---
+++ 01_basics/numbers_8.js
@@ -0,0 +1,10 @@
+const num = 4432
+console.log(num);
+
+const balance = new Number(765300)
+console.log(balance);// [Number: 765300]
+console.log(balance.toString());// 765300 -> returns a string representation of an object
+console.log(balance.toLocaleString('en-IN'));
+ 
+const num2 = 8453.859
+console.log(num2.toPrecision(5));
